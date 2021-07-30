6fbc220c06f56af29d3d0ed7dda3f50bc43bdd77
vichuge <victor.hugo.pacheco.flores@gmail.com>
2021-07-30 11:31:27 -0500

    Try HashRouter to fix gh-pages issue

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
@@ -18,9 +18,9 @@ const store = createStore(
 const rootElement = document.getElementById('root');
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </Provider>,
   rootElement,
 );
